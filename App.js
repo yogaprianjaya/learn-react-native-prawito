409ebe6fb7f58947c7e95239cb276c82f6d22c0c
Yoga Prianjaya <yogaprianjaya@gmail.com>
2022-02-01 20:16:36 +0700

    class component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image, TextInput } from 'react-native';
 
 const App = () => {
@@ -9,16 +9,34 @@ const App = () => {
       <Name />
       <Photo />
       <TextInput style={{borderWidth: 1}} />
+      <BoxGreen />
+      <Profile />
     </View>
   );
 };
 
 const Name = () => {
-  return <Text>Yoga Prianjaya</Text>
+  return <Text>Yoga Prianjaya & Neng Sari</Text>
 };
 
 const Photo = () => {
   return <Image source={{uri: 'https://placeimg.com/100/100/tech'}} style={{width: 100, height: 100}} />
+}
+
+class BoxGreen extends Component {
+  render(){
+    return <Text>Component Class</Text>
+  };
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{uri: 'https://placeimg.com/100/100/animals'}} style={{width: 100, height: 100}} />
+        <Text>Ini Hewan</Text>
+      </View>);
+  };
 }
 
 export default App;
