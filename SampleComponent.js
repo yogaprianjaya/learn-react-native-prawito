29ed6deb56e93f1e8049fd4e025aba594fd5bef5
Yoga Prianjaya <yogaprianjaya@gmail.com>
2022-02-01 21:17:12 +0700

    export dan import component

---
+++ SampleComponent.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {View, Text, Image, TextInput} from 'react-native';
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: 'red'}} />
+      <Text>Hello !</Text>
+      <Name />
+      <Photo />
+      <TextInput style={{borderWidth: 1}} />
+      <BoxGreen />
+      <Profile />
+    </View>
+  );
+};
+
+const Name = () => {
+  return <Text>Yoga Prianjaya & Neng Sari</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/tech'}}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Component Class</Text>;
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/animals'}}
+          style={{width: 100, height: 100}}
+        />
+        <Text>Ini Hewan</Text>
+      </View>
+    );
+  }
+}
+
+export default SampleComponent;
---
+++ StylingReactNativeComponent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import headphone from './headphone.jpg';
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View style={{width: 100, height: 100, backgroundColor: 'green', borderWidth: 2, borderColor: 'blue', marginTop: 20, marginLeft: 20}} />
+      <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212, borderRadius: 8}}>
+        <Image source={headphone} style={{width: 188, height: 107, borderRadius: 8}} />
+        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>New Macbook Pro 2019</Text>
+        <Text style={{fontSize: 12, fontWeight: 'bold', color: '#f2994a', marginTop: 12}}>Rp. 25.000.000</Text>
+        <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>Jakarta Barat</Text>
+        <View style={{backgroundColor: '#6fcf97', paddingVertical: 6, borderRadius: 25, marginTop: 20}}>
+          <Text style={{fontSize: 14, fontWeight: '600', color: 'white', textAlign: 'center'}}>Beli</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+  
+const styles = StyleSheet.create ({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 40
+  }
+});
+
+export default StylingReactNativeComponent;
