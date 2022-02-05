2fa103f528737daf8bc0b1a14ad68aa836effa94
Yoga Prianjaya <yogaprianjaya@gmail.com>
2022-02-05 09:44:44 +0700

    tampilan api

--- src/pages/CallAPIVanila/index.js
+++ src/pages/CallAPIVanila/index.js
@@ -1,33 +1,52 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const CallAPIVanila = () => {
-    useEffect(() => {
-        // Sample Method GET
+    // useEffect(() => {
+    //     // Sample Method GET
+    //     fetch('https://reqres.in/api/users/2')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+
+    //     // Sample Method POST
+    //     const dataAPI = {
+    //         name: "morpheus",
+    //         job: "leader"
+    //     };
+    //     console.log('data object: ', dataAPI);
+    //     console.log('data stringify: ', JSON.stringify(dataAPI));
+    //     fetch('https://reqres.in/api/users', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(dataAPI)
+    //     })
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+    // }, []);
+    const [dataUser, setDataUser] = useState({
+        first_name: '',
+        email: ''
+    });
+
+    const getData = () => {
+        console.log
         fetch('https://reqres.in/api/users/2')
         .then(response => response.json())
-        .then(json => console.log(json))
-
-        // Sample Method POST
-        const dataAPI = {
-            name: "morpheus",
-            job: "leader"
-        };
-        console.log('data object: ', dataAPI);
-        console.log('data stringify: ', JSON.stringify(dataAPI));
-        fetch('https://reqres.in/api/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataAPI)
+        .then(json => {
+            setDataUser(json.data);
         })
-        .then(response => response.json())
-        .then(json => console.log(json))
-    }, []);
+    }
+
     return (
         <View style={style.wrapper}>
             <Text style={style.textTitle}>Get API Vanila JS</Text>
+            <Button title="Get Data" onPress={getData} />
+            <Text style={{textAlign: 'center', marginTop: 10}} >Respone Get Data</Text>
+            <View style={style.line} />
+            <Text>Nama : {dataUser.first_name}</Text>
+            <Text>Email : {dataUser.email}</Text>
         </View>
     );
 };
@@ -36,5 +55,6 @@ export default CallAPIVanila;
 
 const style = StyleSheet.create({
     wrapper: {padding: 20},
-    textTitle: {textAlign: 'center'}
+    textTitle: {textAlign: 'center', marginBottom: 20},
+    line: {height: 2, backgroundColor: 'black'}
 })
